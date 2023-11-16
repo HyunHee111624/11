de0b61c9e0c18fc259528b0937b6c09d2e81047b
HyunHee111624 <144989249+HyunHee111624@users.noreply.github.com>
2023-11-16 15:59:52 +0900

    04

--- main.c
+++ main.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-void swap (int *x, int *y)
-{
-     int temp;
-     temp = *x;
-     *x = *y;
-     *y = temp;
-}
 
 int main(int argc, char *argv[])
 {
-  int a = 3;
-  int b = 5;
-  swap(&a,&b);
+  int i;
+  int average = 0;
   
-  printf("a : %i, b : %i\n", a, b);
-
+  int grade[5];
+  
+  for (i=0; i<5; i++)
+  {
+      printf("input value [%i] = ", i);
+      scanf("%d", &grade[i]);
+  }
+  
+  
+  for (i=0; i<5; i++)
+  {
+      printf("grade[%i] = %i\n",i, *(grade+i));
+      average = average + *(grade+i);
+  }
+  
+  printf("average = %i\n", average/5);
+  
   system("PAUSE");	
   return 0;
 }
